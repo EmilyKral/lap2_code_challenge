72915e60a2d64db5c0f205a629594d32206f44fe
@rajx1 <92103511+Rajx1@users.noreply.github.com>
2021-12-08 14:51:54 +0000

    removed uneeded files, added link generation

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,19 +1,29 @@
 const serverAPI = 'http://localhost:3000'
 const postForm = document.querySelector('#post-form');
+const displayOutput = document.querySelector('#display-output')
 const output = document.querySelector('#output')
 
 // Bind event listeners
 postForm.addEventListener('submit', postEntry)
 
+// render status
+function display(message) {
+    output.textContent = message;
+    output.style.display = 'block';
+}
 
 // create post
-async function createPost(post) {
+async function createPost(postData) {
     try {
-        const res = await axios.post(`${serverAPI}/posts/`, post)
-        console.log(res.data)
+        const res = await axios.post(`${serverAPI}/posts/`, postData);
+        const postLink = `${serverAPI}/posts/${res.data.post.id}`;
+        const anchor = document.createElement('a');
+        anchor.setAttribute('href', postLink);
+        anchor.innerText = postLink;
+        displayOutput.appendChild(anchor);
+        display(`Your post has been created, check out the link!`)
     } catch(err) {
-        output.textContent = `Oops! there was an issue: ${err.message}`;
-        output.style.display = 'block';
+        display(`Oops! there was an issue: ${err.message}`);
     }
 }
 
